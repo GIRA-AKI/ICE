ff5238be76e16e4fc04a471f96b07d4826858c92
GIRA-AKI <roger.2020.y@gmail.com>
2024-04-11 10:33:13 +0700

    test

---
+++ app/global-error.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { button } from "@nextui-org/react";
+import { createBootstrapComponent } from "react-bootstrap/esm/ThemeProvider";
+ 
+const error = ({error , reset} : {error: Error ; reset: ()=>void}) => {
+  return (
+    <main className="flex flex-col items-center h-screen justify-center content-center ">
+      <div className="my-5 text-xl h2 ">There was a problem</div>
+      <div className="my-5"><span className="h5">the reason is :</span><span className="text-lg"> {error.message || 'Something went wrong'}</span></div>
+      <div className="my-5"> <button onClick={reset} className="btn btn-primary">Try again</button></div>
+    </main>
+  )
+  return <div>error</div>
+}
+
+export default error
---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const nf = () => {
+  return (
+    <div className='grid  content-center bg-cyan-900 h-screen items-center justify-around'>
+        <h1 className='mx-auto text-white'>Not found page hubbbbb</h1>
+        <p className='mx-auto text-white'>404 | this page cloud not be found</p>
+    </div>
+  )
+}
+
+export default nf
